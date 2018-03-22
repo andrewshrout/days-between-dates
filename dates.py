732e48d18bb2b55cc726f90fe0bd22ad219e4488
andrewshrout <andrewshrout1@gmail.com>
2018-03-22 12:35:57 -0700

    Update dates.py

--- dates.py
+++ dates.py
@@ -52,7 +52,6 @@ def daysBetweenDates(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar."""
-    # program defensively! Add an assertion if the input is not valid!
     assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
     days = 0
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
